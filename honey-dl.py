5543cbf17053737c8a3c369316487296517440b9
Muhammed S. <mukonqi@gmail.com>
2025-11-23 18:28:55 +0300

    feat: Add timeout support with retrying and more

--- honey-dl.py
+++ honey-dl.py
@@ -9,30 +9,22 @@ import requests
 from bs4 import BeautifulSoup, FeatureNotFound
 
 
-def parse_json_callback(ctx, param, value):
-    if not value:
-        return {}
-    if isinstance(value, dict):
-        return value
-    try:
-        return json.loads(value)
-    except json.JSONDecodeError as e:
-        raise click.BadParameter(f"Invalid JSON for {param.name}: {e}") from None
-
-
 def download_image(
     date: str,
-    src: str,
+    url: str,
     domain: str,
     add_dates: bool,
     create_folders: bool,
     force: bool,
     headers: dict,
     proxies: dict,
+    retries: int,
+    timeout: int,
 ):
-    response = requests.get(src, headers=headers, proxies=proxies)
-    if response.status_code == 200:
-        url_path = urlparse(src).path
+    content = get_content(url, headers, proxies, retries, timeout)
+
+    if content:
+        url_path = urlparse(url).path
         filename = os.path.basename(url_path)
 
         if add_dates:
@@ -47,28 +39,53 @@ def download_image(
         os.makedirs(os.path.dirname(path), exist_ok=True)
         if not os.path.isfile(path) or force:
             with open(path, "wb") as f:
-                f.write(response.content)
+                f.write(content)
             print(f"📥 Downloaded: {filename}")
         else:
-            print(f"⏭️  Skipped (already exists): {filename}")
-    else:
-        print(f"❌ Failed to download ({response.status_code}): {src}")
+            print(f"⏭️ Skipped (already exists): {filename}")
+        return True
 
 
 def ends_with_git(url: str):
     return bool(re.search(r"&git=\d+$", url))
 
 
-def get_soup(url: str, headers: dict, proxies: dict):
-    response = requests.get(url, headers=headers, proxies=proxies)
-    if response.status_code == 200:
+def get_content(url: str, headers: dict, proxies: dict, retries: int, timeout: int):
+    i = 0
+
+    while i <= retries:
         try:
-            return BeautifulSoup(response.content, "lxml")
+            response = requests.get(url, headers=headers, proxies=proxies, timeout=timeout)
+            if response.status_code == 200:
+                return response.content
+            else:
+                print(f"❌ Failed to get: {url}, {response.status_code}")
+                return False
+        except requests.exceptions.Timeout:
+            if i == retries:
+                print(f"❌ Timed out: {url}")
+                return False
+            else:
+                print(f"🔁 Timed out, retrying ({i + 1}/{retries}): {url}")
+            i += 1
+        except requests.exceptions.RequestException as e:
+            print(f"❌ Request error: {url}, {e}")
+            if i == retries:
+                return False
+            print(f"🔁 Request error, retrying ({i + 1}/{retries}): {url}")
+            i += 1
+
+
+def get_soup(url: str, headers: dict, proxies: dict, retries: int, timeout: int):
+    content = get_content(url, headers, proxies, retries, timeout)
+
+    if content:
+        try:
+            return BeautifulSoup(content, "lxml")
         except FeatureNotFound:
-            return BeautifulSoup(response.content, "html.parser")
+            return BeautifulSoup(content, "html.parser")
     else:
-        print(f"❌ Failed to fetch {url}, status code: {response.status_code}")
-        return None
+        return False
 
 
 def handle_page(
@@ -79,13 +96,28 @@ def handle_page(
     force: bool,
     headers: dict,
     proxies: dict,
-    threads: int,
+    retries: int,
+    timeout: int,
+    workers: int,
 ):
-    with ThreadPoolExecutor(max_workers=threads) as executor:
+    with ThreadPoolExecutor(max_workers=workers) as executor:
         for img in results:
             date = img.get("alt")
             src = img.get("src")
-            executor.submit(download_image, date, src, domain, add_dates, create_folders, force, headers, proxies)
+            executor.submit(
+                download_image, date, src, domain, add_dates, create_folders, force, headers, proxies, retries, timeout
+            )
+
+
+def parse_json_callback(ctx, param, value):
+    if not value:
+        return {}
+    if isinstance(value, dict):
+        return value
+    try:
+        return json.loads(value)
+    except json.JSONDecodeError as e:
+        raise click.BadParameter(f"❌ Invalid JSON for {param.name}: {e}") from None
 
 
 @click.command()
@@ -116,7 +148,9 @@ def handle_page(
     callback=parse_json_callback,
     help="Proxy settings as JSON",
 )
-@click.option("-t", "--threads", default=10, type=int, help="Number of download threads")
+@click.option("-r", "--retries", default=5, type=int, help="Number of retries per request")
+@click.option("-t", "--timeout", default=5, type=int, help="Timeout time per request")
+@click.option("-w", "--workers", default=30, type=int, help="Number of downloader workers")
 @click.argument("url")
 def main(
     add_dates: bool,
@@ -124,7 +158,9 @@ def main(
     force: bool,
     headers: dict,
     proxies: dict,
-    threads: int,
+    retries: int,
+    timeout: int,
+    workers: int,
     url: str,
 ):
     try:
@@ -135,19 +171,19 @@ def main(
             f.write("*")
 
         if ends_with_git(url):
-            print("📖 Single-page mode: Starting download...")
-            soup = get_soup(url, headers, proxies)
+            print("📄 Single-page mode: Starting download...")
+            soup = get_soup(url, headers, proxies, retries, timeout)
             if soup is None:
                 return
             results = soup.find_all("img", class_="img-thumbnail")
-            handle_page(results, domain, add_dates, create_folders, force, headers, proxies, threads)
+            handle_page(results, domain, add_dates, create_folders, force, headers, proxies, retries, timeout, workers)
             print("✅ Downloading completed!")
         else:
             page = 1
             while True:
                 paginated_url = f"{url}&git={page}"
                 print(f"📖 Processing page: {page}")
-                soup = get_soup(paginated_url, headers, proxies)
+                soup = get_soup(paginated_url, headers, proxies, retries, timeout)
                 if soup is None:
                     print(f"❌ Failed to retrieve page: {paginated_url}")
                     break
@@ -157,11 +193,13 @@ def main(
                     print("✅ Downloading completed!")
                     break
 
-                handle_page(results, domain, add_dates, create_folders, force, headers, proxies, threads)
+                handle_page(
+                    results, domain, add_dates, create_folders, force, headers, proxies, retries, timeout, workers
+                )
                 page += 1
     except KeyboardInterrupt:
-        print("\n🛑 Interrupted by user. Exiting...")
-        return
+        print("\n🛑 Interrupted by user. Trying to exit..")
+        exit(0)
 
 
 if __name__ == "__main__":
